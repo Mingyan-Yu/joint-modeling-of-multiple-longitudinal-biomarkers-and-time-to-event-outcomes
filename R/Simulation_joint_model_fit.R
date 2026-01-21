40a28c57ee47bd524ee89667cc9dd61dc87f0b2c
Mingyan-Yu <79581919+Mingyan-Yu@users.noreply.github.com>
2026-01-21 10:24:58 -0500

    Update Simulation_joint_model_fit.R

--- R/Simulation_joint_model_fit.R
+++ R/Simulation_joint_model_fit.R
@@ -1,1 +1,20 @@
 ###### Joint modeling of longitudinal biomarkers and time-to-event outcomes ######
+
+## load packages
+library(nimble)
+library(data.table)
+library(coda)
+library(parallel)
+library(nlme)
+library(GLMMadaptive)
+library(dplyr)
+
+k <- Sys.getenv("SLURM_ARRAY_TASK_ID")
+print(k)
+
+set.seed(20251210+as.numeric(k)*100)
+
+## create 3 parallel clusters
+cl <- makeCluster(3)
+
+## load simulated data
